96dd11a40d2e620573ab581b1a825057e5e8d55c
Aurelie Dufour <aure.dufour@gmail.com>
2017-07-18 21:48:36 -0400

    configuration on the files + architectureof the app

--- src/index.js
+++ src/index.js
@@ -1,8 +1,29 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from '../App';
 import registerServiceWorker from './registerServiceWorker';
+import { createStore, applyMiddleware } from 'redux';
+import { Provider } from 'react-redux';
+import { createEpicMiddleware } from 'redux-observable';
+import { createLogger } from 'redux-logger';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import rootReducer from './redux/reducers';
+import rootEpic from './redux/epics';
+import App from './components/App';
+
+import './index.css';
+
+const loggerMiddleware = createLogger()
+
+const store = createStore(
+  rootReducer,
+  applyMiddleware(createEpicMiddleware(rootEpic), loggerMiddleware),
+);
+
+const ReduxApp = () => (
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+ReactDOM.render(<ReduxApp />, document.getElementById('root'));
 registerServiceWorker();
