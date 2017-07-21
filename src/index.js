48adec0a0832bb0f1fa496f49a1b49b4b1779def
Aurelie Dufour <aure.dufour@gmail.com>
2017-07-20 20:00:48 -0400

    comment out createEpicMiddleware and rootEpic

--- src/index.js
+++ src/index.js
@@ -3,11 +3,11 @@ import ReactDOM from 'react-dom';
 import registerServiceWorker from './registerServiceWorker';
 import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
-import { createEpicMiddleware } from 'redux-observable';
+// import { createEpicMiddleware } from 'redux-observable';
 import { createLogger } from 'redux-logger';
 
 import rootReducer from './redux/reducers';
-import rootEpic from './redux/epics';
+// import rootEpic from './redux/epics';
 import App from './components/App';
 
 import './index.css';
