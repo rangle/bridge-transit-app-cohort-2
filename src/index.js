48bdbdf3c5de54830de754293df47ed59693291c
Meltem <melltemkilic@gmail.com>
2017-07-21 19:31:57 -0400

    Add react-router and react-router-redux

--- src/index.js
+++ src/index.js
@@ -5,10 +5,13 @@ import { createStore, applyMiddleware } from 'redux';
 import { Provider } from 'react-redux';
 // import { createEpicMiddleware } from 'redux-observable';
 import { createLogger } from 'redux-logger';
+import { Router, Route, browserHistory } from 'react-router';
+import { syncHistoryWithStore } from 'react-router-redux'
 
 import rootReducer from './redux/reducers';
 // import rootEpic from './redux/epics';
 import App from './components/App';
+import Foo from './components/Foo';
 
 import './index.css';
 
@@ -19,9 +22,14 @@ const store = createStore(
   applyMiddleware(loggerMiddleware),
 );
 
+const history = syncHistoryWithStore(browserHistory, store);
+
 const ReduxApp = () => (
   <Provider store={store}>
-    <App />
+    <Router history={history}>
+      <Route path="/" component={App}/>
+      <Route path="/foo" component={Foo}/> 
+    </Router>
   </Provider>
 );
 
