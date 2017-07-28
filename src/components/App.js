52cf2f28d0e61955a091ef17101f4693039264f7
desaree <srhboo@gmail.com>
2017-07-28 10:54:06 -0400

    add category routing

--- src/components/App.js
+++ src/components/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
-
+import { Link } from 'react-router';
 import { inputChange, setSearchKeyword, getCategories } from './../redux/actions';
 import { SearchInput } from './SearchInput';
 import { CategoriesList } from './CategoriesList';
@@ -18,21 +18,32 @@ class App extends Component {
       getCategories,
     } = this.props;
 
+    // this connects props to children (for example, CategoriesList)
+    const childrenWithProps = React.cloneElement(this.props.children, {...this.props});
+
     return (
       <div className="App">
-        <SearchInput {...this.props} label="Search Events"/>
+
+        <div>
+          <Link to="/category/2">Category 2</Link> | <Link to="/">Home</Link>
+        </div>
+
+        <div>
+          <SearchInput {...this.props} label="Search Events"/>
           <Button
-            className='hello'
-            aria-label='hello'
-            content='hello'
-            handleClick={ () => setSearchKeyword(searchInput) }
+          className='hello'
+          aria-label='hello'
+          content='hello'
+          handleClick={ () => setSearchKeyword(searchInput) }
           />
-          <CategoriesList allCategories={allCategories} selectedCategories={selectedCategories} />
-            <button
-              onClick={ () => getCategories() }
-            >
-              Fetch Categories
-            </button>
+          <button
+          onClick={ () => getCategories() }
+          >
+          Fetch Categories
+          </button>
+          { childrenWithProps }
+        </div>
+
       </div>
     );
   }
---
+++ src/components/CategoryPage.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const CategoryPage = (props) => (
+    <div>
+        <h2>Category Type {props.params.categoryID} Events near Toronto, Ontario</h2>
+        <div>List of Event Items Component goes here</div>
+    </div>
+);
