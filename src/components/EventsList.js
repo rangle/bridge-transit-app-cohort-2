41995ee504528f8743aada5dd397c4c8899766eb
Aurelie Dufour <aure.dufour@gmail.com>
2017-07-30 16:08:01 -0400

    change the items and listItems for event and event-list

---
+++ src/components/EventsList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import Event from './Event';
+
+export const EventsList = events => {
+  return(
+    <ul className='events-list'>
+      { events.map( event =>  <Event {...event}/> )};
+    </ul>
+  )
+}
