3d5f5988d597098118772c82e69c8fa16cd633a6
cgolinski <cgolinski@users.noreply.github.com>
2017-08-02 16:30:39 -0400

    converted CategoryPage to class-based component, and fetch events on initial render

--- src/components/CategoryPage.js
+++ src/components/CategoryPage.js
@@ -1,21 +1,27 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { EventsList } from './EventsList';
 
+export class CategoryPage extends Component {
+  componentDidMount() {
+    this.props.getEvents();
+  }
 
-
-export const CategoryPage = (props) => (
-  <div>
-    {
-      props.allCategories.map(category => category.id === props.params.categoryID 
-      ? <h2>{category.name} Events near Toronto, Ontario</h2> 
-      : null)
-    }
-    <button className="fetch-events" onClick={ () => props.getEvents() }>
-      Fetch Events
-    </button>
-    {props.events 
-      ? <EventsList events={props.events} />
-      : null
-    }
-  </div>
-); 
+  render() {
+    return (
+      <div>
+        {
+          this.props.allCategories.map(category => category.id === this.props.params.categoryID 
+          ? <h2>{category.name} Events near Toronto, Ontario</h2> 
+          : null)
+        }
+        <button className="fetch-events" onClick={ () => this.props.getEvents() }>
+          Fetch Events
+        </button>
+        {this.props.events 
+          ? <EventsList events={this.props.events} />
+          : null
+        }
+      </div>
+    );
+  }
+}
