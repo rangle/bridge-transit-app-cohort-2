f64cf42e111e66bb874895ffa49a982e2e25c422
cgolinski <cgolinski@users.noreply.github.com>
2017-08-02 17:59:41 -0400

    fixing spacing

--- src/redux/epics/event.epics.js
+++ src/redux/epics/event.epics.js
@@ -6,17 +6,17 @@ import 'rxjs/add/operator/mergeMap';
 const BASE_ENDPOINT = `https://www.eventbriteapi.com/v3/events/search/?token=${EVENTBRITE_API_KEY}&location.address=toronto&categories=`;
 
 export const getEventEpic = (action$, store) =>
-    action$.ofType(ACTION_TYPES.GET_EVENTS)
-      .mergeMap(action =>
-        Observable.ajax({
-          url: `${BASE_ENDPOINT}${
-            store.getState().routing.locationBeforeTransitions.pathname
-            .replace('/category/', '')
-          }`,
-          crossDomain: true
-        })
-          .map(({ response }) => ({
-            type: ACTION_TYPES.SET_EVENTS,
-            payload: response,
-          }))
-      );
+  action$.ofType(ACTION_TYPES.GET_EVENTS)
+    .mergeMap(action =>
+      Observable.ajax({
+        url: `${BASE_ENDPOINT}${
+          store.getState().routing.locationBeforeTransitions.pathname
+          .replace('/category/', '')
+        }`,
+        crossDomain: true
+      })
+        .map(({ response }) => ({
+          type: ACTION_TYPES.SET_EVENTS,
+          payload: response,
+        }))
+    );
