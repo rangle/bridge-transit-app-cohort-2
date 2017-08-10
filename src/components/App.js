79ab7809fe80fc436a78e66d17496814d9e3c91e
desaree <srhboo@gmail.com>
2017-08-10 17:17:11 -0400

    add category and event errors to props in App

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,10 @@
 import React, { Component } from 'react';
 import { connect } from 'react-redux';
 import { inputChange, setSearchKeyword, getCategories, getEvents, setAddresses, selectAddress, saveAddress, addressInputChange, showAddressWindow, hideAddressWindow } from './../redux/actions';
-import { Navigation } from './Navigation';
+import { SearchInput } from './SearchInput';
+import { Address } from './Address';
 import { AddressWindow } from './AddressWindow';
+import Button from './Button';
 
 class App extends Component {
   componentDidMount() {
@@ -10,13 +12,30 @@ class App extends Component {
   }
 
   render() {
+    const {
+      searchInput,
+      setSearchKeyword,
+    } = this.props;
+
     // this connects props to children (for example, CategoriesList)
     const childrenWithProps = React.cloneElement(this.props.children, {...this.props});
 
     return (
       <div className="App">
-        <Navigation {...this.props}/>
-        { this.props.displayAddressWindow ? <AddressWindow {...this.props} /> : null}
+        {this.props.displayAddressWindow ? <AddressWindow {...this.props} /> : null}
+        <div className="App-header">
+          <Address {...this.props} />
+          <h1 className="header-catch-phrase">Never miss the next event</h1>
+          <div className="search-input-container">
+            <SearchInput {...this.props} label="Search Events"/>
+            <Button
+              className='button is-primary is-large'
+              aria-label='search button'
+              content='Search'
+              handleClick={ () => setSearchKeyword(searchInput) }
+            />
+          </div>
+        </div>
         {childrenWithProps}
       </div>
     );
@@ -27,11 +46,14 @@ const connectConfig = connect(state => ({
   searchInput: state.searchInput,
   searchKeyword: state.category.searchKeyword,
   allCategories: state.category.categories,
-  selectedCategories: state.category.categories && state.category.categories.length > 0 
+  selectedCategories: state.category.categories 
     ? state.category.categories.filter(category => 
       category.name.toLowerCase().includes(state.category.searchKeyword.toLowerCase())) 
     : null,
+  chosenCategory: state.category.chosenCategory,
+  categoriesError: state.category.didInvalidate,
   events: state.events.events,
+  eventsError: state.events.didInvalidate,
   addresses: state.address.addresses,
   selectedAddress: state.address.selectedAddress,
   savedAddress: state.address.savedAddress,
