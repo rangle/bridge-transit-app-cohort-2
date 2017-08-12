17b53ae6b380d279d5e3e0777595b3c0c27504be
cgolinski <cgolinski@users.noreply.github.com>
2017-08-12 11:52:56 -0400

    set default location to toronto

--- src/components/MyMap.js
+++ src/components/MyMap.js
@@ -1,11 +1,9 @@
 import React from 'react';
-import { withGoogleMap, GoogleMap, /*Marker*/ } from "react-google-maps";
-
-const googleMapURL="https://maps.googleapis.com/maps/api/js?v=3.27&libraries=places,geometry&key=AIzaSyC4sITGIQEKBi1ejAZxazvwZlrk1pi7OeA";
+import { withGoogleMap, GoogleMap } from "react-google-maps";
 
 export const MyMap = withGoogleMap(props => (
   <GoogleMap
-    defaultZoom={8}
-    defaultCenter={{ lat: -34.397, lng: 150.644 }}
+    defaultZoom={12}
+    defaultCenter={{ lat: 43.653, lng: -79.383 }}
   />
 ));
