b27e4240a5758ed5113d191cd29b07223a8746dd
Meltem <melltemkilic@gmail.com>
2017-08-13 16:51:02 -0400

    Display the event date properly

--- src/components/Event.js
+++ src/components/Event.js
@@ -1,6 +1,16 @@
 import React from 'react';
 
 export const Event = event => {
+  const eventDateInstance = new Date(event.start.local)
+  const eventDateDay = eventDateInstance.toLocaleString('en-us', { day: "numeric" })
+  const eventDateWeekday = eventDateInstance.toLocaleString('en-us', { weekday: "short" })
+  const eventDateMonth = eventDateInstance.toLocaleString('en-us', { month: "short" })
+  const eventDateHour = eventDateInstance.toLocaleString('en-us', { hour: "numeric", hour12: false })
+  let eventDateMinute = eventDateInstance.toLocaleString('en-us', { minute: "numeric" })
+  eventDateMinute = eventDateMinute === '0'
+    ? eventDateMinute = `${eventDateMinute}0`
+    : eventDateMinute
+
   return(
     <li className='event-card'>
       <a href={event.url}>
@@ -9,7 +19,7 @@ export const Event = event => {
             {event.logo ? <img src={event.logo.url}/> : null}
           </div>
           <div className='event-info'>
-            <p className='event-info--time'>{event.start.local}</p>
+            <p className='event-info--time'>{`${eventDateWeekday}, ${eventDateDay} ${eventDateMonth} ${eventDateHour}:${eventDateMinute}`}</p>
             <h3 className='event-info--title'>{event.name.text}</h3>
             <p className='event-info--location'>{event.start.timezone}</p>
             <span className='event-info--price'>{
