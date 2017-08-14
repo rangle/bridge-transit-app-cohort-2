c37e1fd6b5aa98000095ddcd45575ec8061a9c48
Meltem <melltemkilic@gmail.com>
2017-08-13 20:41:36 -0400

    Change the text of hard-coded event tag

--- src/components/Event.js
+++ src/components/Event.js
@@ -29,7 +29,7 @@ export const Event = event => {
             <p className='event-info--location'>{event.start.timezone}</p>
             <div className="event-info--tag">
               <span>
-                #Festival
+                #Event
               </span>
             </div>
           </div>
