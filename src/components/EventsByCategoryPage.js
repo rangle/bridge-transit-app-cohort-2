5372c20019f5fc51b184b87dee4200cbca767020
Meltem <melltemkilic@gmail.com>
2017-08-14 22:33:21 -0400

    Create spinner component. Give it a styling and wrap it in an overlay

--- src/components/EventsByCategoryPage.js
+++ src/components/EventsByCategoryPage.js
@@ -1,15 +1,19 @@
 import React from 'react';
 import { EventsList } from './EventsList';
+import { Spinner } from './Spinner';
 
-export const EventsByCategoryPage = (props) => (
-  <div className="events-by-category-page">
-    {props.allCategories.map(category => category.id === props.params.categoryID
-      ? <h2>{category.name} Events near Toronto, Ontario</h2>
-      : null)
-    }
-    {props.events
-      ? <EventsList events={props.events} />
-      : props.eventsError ? 'Sorry, there seems to be an error. Please try again later.' : null
-    }
-  </div>
-);
+export const EventsByCategoryPage = (props) => {
+  return (
+    props.eventsFetching ? <div className="overlay"><Spinner/></div> :
+    <div className="events-by-category-page">
+      {props.allCategories.map(category => category.id === props.params.categoryID
+        ? <h2>{category.name} Events near Toronto, Ontario</h2>
+        : null)
+      }
+      {props.events
+        ? <EventsList events={props.events} />
+        : props.eventsError ? 'Sorry, there seems to be an error. Please try again later.' : null
+      }
+    </div>
+  )
+};
---
+++ src/components/Spinner.js
@@ -0,0 +1,8 @@
+import React from 'react';
+
+export const Spinner = () => {
+    return <div className="spinner">
+        <span>Loading</span>
+        <div className="spinner-animation"></div>
+    </div>;
+}
