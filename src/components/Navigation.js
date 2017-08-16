b991b3750da3b532a1e5eef36add4c9e2c2f06bc
cgolinski <cgolinski@users.noreply.github.com>
2017-08-15 20:58:16 -0400

    fixed home link in nav

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import { Address } from './Address';
+import { IndexLink } from 'react-router';
 
 
 export const Navigation = props => (
     <nav>
       <ul className="navbar-items">
-        <li className="navbar-items--home"><a href='/'>TransitTO</a></li>
+        <li className="navbar-items--home"><IndexLink to='/'>TransitTO</IndexLink></li>
         <li className="navbar-items--address"><Address {...props} /></li>
       </ul>
     </nav>
