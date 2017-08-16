a584a1847cd40ef83bb962da95f0e533c4a77299
desaree <srhboo@gmail.com>
2017-08-15 23:18:07 -0400

    update the event details component to render based on event state

--- src/components/EventDetails.js
+++ src/components/EventDetails.js
@@ -1,42 +1,43 @@
 import React from 'react';
 import { Button } from './Button';
 import { SimpleMap } from './SimpleMap';
+import { Spinner } from './Spinner';
 
 export const EventDetails = (props) => {
   return(
-    <div className='event-details-card'>
-      <div className="event-details-container">
-        <h3>{props.event.name.text}</h3>
-        {
-          props.event.logo.url ? 
-          <img alt="Event" src={props.event.logo.url} />
-          : null
-        }
-        <h3>{props.event.name.text}</h3>
-        <img alt="Event" src={props.event.logo.url} />
+    props.eventError ?  <div>Sorry, there seems to be an error. Please try again later.</div>
+    : props.eventFetching ? <div className="overlay"><Spinner/></div> 
+      : <div className='event-details-card'>
+        <div className="event-details-container">
+          <h3>{props.event.name.text}</h3>
+          {
+            props.event.logo.url ? 
+            <img alt="Event" src={props.event.logo.url} />
+            : null
+          }
+        </div>
+        <div className='event-details-container'>
+          <p className='event-details--time'>{props.event.start.local} to {props.event.end.local}</p>
+          <p className='event-details--description'>{props.event.description.text}</p>
+          <p className='event-details--price'>{
+            props.event.is_free ? <span>Free</span> :
+              <Button
+                className='button is-primary is-large'
+                content='Buy Tickets'
+                aria-label='Buy Tickets'
+                handleClick= { () => window.location.assign(props.event.url)}
+              />
+          } </p>
+        </div>
+        <SimpleMap
+          containerElement={
+            <div className='map-container-element' />
+          }
+          mapElement={
+            <div className='map-element' />
+          }
+        />
       </div>
-      <div className='event-details-container'>
-        <p className='event-details--time'>{props.event.start.local} to {props.event.end.local}</p>
-        <p className='event-details--description'>{props.event.description.text}</p>
-        <p className='event-details--price'>{
-          props.event.is_free ? <span>Free</span> :
-            <Button
-              className='button is-primary is-large'
-              content='Buy Tickets'
-              aria-label='Buy Tickets'
-              handleClick= { () => window.location.assign(props.event.url)}
-            />
-        } </p>
-      </div>
-      <SimpleMap
-        containerElement={
-          <div className='map-container-element' />
-        }
-        mapElement={
-          <div className='map-element' />
-        }
-      />
-    </div>
   )
 };
 
