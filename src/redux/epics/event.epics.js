460eb421829605b06a7a3b50c9ed04d24a3534f3
desaree <srhboo@gmail.com>
2017-08-15 20:58:51 -0400

    refactor API code for eventbrite to include requesting single events

--- src/redux/epics/event.epics.js
+++ src/redux/epics/event.epics.js
@@ -3,9 +3,13 @@ import { EVENTBRITE_API_KEY } from '../../constants/keys';
 import { Observable } from 'rxjs';
 import 'rxjs/add/operator/mergeMap';
 
-const BASE_ENDPOINT = `https://www.eventbriteapi.com/v3/events/search/?token=${EVENTBRITE_API_KEY}&location.address=toronto&categories=`;
-const reCategoryPath = /^\/category\/\d\d\d$/;
+const BASE_ENDPOINT = `https://www.eventbriteapi.com/v3/events/`;
+const API_KEY_PATH = `?token=${EVENTBRITE_API_KEY}`;
 
+// SECTION: Events by category
+const CATEGORY_QUERY_PATH = '&location.address=toronto&categories=';
+const CATEGORY_METHOD_PATH = 'search/';
+const reCategoryPath = /^\/category\/\d\d\d$/;
 export const returnEventsActionOnLocationChange = (action$) =>
   action$.ofType('@@router/LOCATION_CHANGE')
     .filter(action => reCategoryPath.test(action.payload.pathname))
@@ -18,7 +22,7 @@ export const getEventsEpic = (action$) =>
   action$.ofType(ACTION_TYPES.GET_EVENTS)
     .mergeMap(action =>
       Observable.ajax({
-        url: `${BASE_ENDPOINT}${action.payload}`,
+        url: `${BASE_ENDPOINT}${CATEGORY_METHOD_PATH}${API_KEY_PATH}${CATEGORY_QUERY_PATH}${action.payload}`,
         crossDomain: true
       })
       .map(({ response }) => ({
@@ -29,4 +33,29 @@ export const getEventsEpic = (action$) =>
         type: ACTION_TYPES.SET_EVENT_INVALIDATE,
         payload: error
       })) 
+    );
+
+// SECTION: Single event
+
+const reEventPath = /^\/event\/\d\d\d$/;
+
+export const returnEventActionOnLocationChange = (action$) =>
+  action$.ofType('@@router/LOCATION_CHANGE')
+    .filter(action => reEventPath.test(action.payload.pathname))
+    .map(action => ({
+        type: ACTION_TYPES.GET_EVENT,
+        payload: action.payload.pathname.replace('/event/', '')
+      }));
+
+export const getEventEpic = (action$) =>
+  action$.ofType(ACTION_TYPES.GET_EVENT)
+    .mergeMap(action =>
+      Observable.ajax({
+        url: `${BASE_ENDPOINT}${action.payload}/${API_KEY_PATH}`,
+        crossDomain: true
+      })
+        .map(({ response }) => ({
+          type: ACTION_TYPES.SET_EVENT,
+          payload: response,
+        }))
     );
