ad6ef0b79417f12737d3e20318e648524bbdce5a
desaree <srhboo@gmail.com>
2017-08-23 23:00:59 -0400

    move the CORS Request function to separate file

--- src/redux/epics/address.epics.js
+++ src/redux/epics/address.epics.js
@@ -1,6 +1,7 @@
 import { ACTION_TYPES } from '../../constants/ActionTypes';
 import { PLACES_KEY } from '../../constants/keys';
 import 'rxjs/add/operator/mergeMap';
+import { doCORSRequest } from '../../utils/doCorsRequest';
 
 //Coordinates of Toronto point
 const TORONTO_COORDS = '43.655336,-79.402816';
@@ -10,28 +11,6 @@ const RADIUS = '30000';
 
 //PLACES ENDPOINT
 const BASE_ENDPOINT = `https://maps.googleapis.com/maps/api/place/textsearch/json?location=${TORONTO_COORDS}&radius=${RADIUS}&key=${PLACES_KEY}`;
-
-// https://cors-anywhere.herokuapp.com/ CORS Everywhere 
-const cors_api_url = 'https://cors-anywhere.herokuapp.com/';
-
-// include this function def for development
-const doCORSRequest = (options) => {
-    return new Promise((resolve, reject) => {
-        var x = new XMLHttpRequest();
-        x.open(options.method, cors_api_url + options.url);
-        x.onload = () => {
-            if (x.status >= 200 && x.status < 300) {
-                resolve(x.responseText);
-            } else {
-                reject(x.statusText);
-            }
-        };
-        if (/^POST/i.test(options.method)) {
-        x.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-        }
-        x.send(options.data);
-})};
-
 
 //TODO: change fetch to Observable.ajax
 export const getAddressEpic = (action$, _, {ajax}) =>
