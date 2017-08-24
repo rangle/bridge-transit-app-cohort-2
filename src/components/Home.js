27b862815e053af53b0cd74f1f203b1e135a5050
Aurelie Dufour <aure.dufour@gmail.com>
2017-08-24 02:39:17 -0400

    Make the header change hero image on load

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,22 @@ import { Button } from './Button';
 import { SearchInput } from './SearchInput';
 import { CategoriesList } from './CategoriesList';
 
+const randomNumber = (max, min) => {
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+let classNumber;
+
+const headerClass = () => {
+  if (!classNumber){
+    classNumber = randomNumber(0 , 7);
+  }
+  return `App-header-${classNumber}`;
+}
+
 export const Home = props => (
   <div className="home-container">
-    <div className="App-header">
+    <div className={`App-header ${headerClass()}`}>
       <h1 className="header-catch-phrase">Never miss the next event</h1>
       <div className="search-input-container">
       <SearchInput {...props} label="Search Events"/>
