0b258a98297809b785652b3416431f19cd71afd7
desaree <srhboo@gmail.com>
2017-08-23 23:01:45 -0400

    add Directions Window component

--- src/components/DirectionsWindow.js
+++ src/components/DirectionsWindow.js
@@ -1,37 +1,52 @@
 import React from 'react';
-import { SimpleMap } from './SimpleMap'; 
 import { DirectionsMap } from './DirectionsMap';
+import { DirectionsPanel } from './DirectionsPanel';
+import { DirectionsLocation } from './DirectionsLocation';
+import { Spinner } from './Spinner';
 
-export const DirectionsWindow = props => (
-    <div className="directions-window">
-        <div className="directions-modal">
-            <button className="close-button" onClick={()=>console.log("close window action")}>
-                <span>&#10005;</span>
-            </button>
-            <h2>Transit Directions</h2>
-            <div className="directions-addresses">
-                <div>
-                   From: {props.savedAddress.formatted_address ? props.savedAddress.formatted_address : 'set your address'}
-                </div>
-                <div>
-                   To: {props.event.venue.address.localized_address_display}
-                </div>
-            </div>
-           <div className="directions-container">
-               <div className="directions-map">
-                    <DirectionsMap
-                        containerElement={
-                            <div className='directions-map-container-element' />
-                        } 
-                        mapElement={
-                            <div className='directions-map-element' />
+export const DirectionsWindow = props => {
+    if (props.directionsFetching){
+      return (<div className="overlay"><Spinner/></div>);
+    } else { 
+        return (
+            <div className="directions-window">
+                <div className="directions-modal">
+                    <button className="close-button" onClick={() => props.hideDirectionsWindow()}>
+                        <span>&#10005;</span>
+                    </button>
+                    <h1>Transit Directions</h1>
+                    <div className="directions-addresses">
+                        <DirectionsLocation label="From:"
+                                            content={props.savedAddress.formatted_address}
+                                            ifNull="Set your address"
+                        />
+                        <DirectionsLocation label="To:"
+                                            content={props.event.venue.address.localized_address_display}
+                                            ifNull="Couldn't find an event address, sorry. "
+                        />
+                    </div>
+
+                    <div className="directions-container">
+                        <div className="directions-map">
+                        { 
+                            props.directions ?
+                            <DirectionsMap
+                                containerElement={
+                                    <div className='directions-map-container-element' />
+                                } 
+                                mapElement={
+                                    <div className='directions-map-element' />
+                                }
+                                directions={
+                                    props.directions
+                                }
+                            />
+                            : null 
                         }
-                    />
-               </div>
-                <div className="directions-panel">
-                    <h3>Instructions here:</h3>
+                        </div>
+                        <DirectionsPanel {...props} />
+                    </div>
                 </div>
-           </div>
-        </div>
-    </div> 
-);
+            </div> 
+    )}
+};
